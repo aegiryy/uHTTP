e04df55eb062be01439c00af790927606aa8dbed
abraham <abraham@abraham.(none)>
2011-03-17 17:21:35 +0800

    test

--- source/cgi.c
+++ source/cgi.c
@@ -1,22 +1,16 @@
-#include "cgi.h"
-#include <stdlib.h>
-#include <string.h>
 #include <stdio.h>
-#include <fcntl.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <sys/types.h>
+#include <stdlib.h>
 #include <memory.h>
 #define BUF_SIZE 1024
 #define PROG_NUM 3  //cgi解释器的表大小
 char* prog_suff[PROG_NUM]={"php","py","sh"};
 char* prog_name[PROG_NUM]={"php-cgi","python","bash"};
 
-
 void do_cgi(const char* path,const char* suffix,const char* params){
 	FILE* fp;
 	char data[BUF_SIZE];
 	char cmd[BUF_SIZE];
+	memset(cmd,'\0',sizeof(cmd));
 	int i=0;
 	int index=-1;
 	for(i=0;i<PROG_NUM;i++){
@@ -34,7 +28,7 @@ void do_cgi(const char* path,const char* suffix,const char* params){
 	strcat(cmd, path);
 	strcat(cmd, " ");
 	strcat(cmd, params);
-	//printf("%s\n\n",cmd);
+	printf("%s\n\n",cmd);
 	if((fp=popen(cmd,"r"))!=NULL){
         	while(fgets(data,sizeof(data),fp)!=NULL){
 			printf("%s",data);
@@ -44,5 +38,6 @@ void do_cgi(const char* path,const char* suffix,const char* params){
 
     
 }
+
 
 
